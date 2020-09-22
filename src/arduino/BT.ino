e57adda1a1e278ff511f06debb85988f9eb9c77c
prax78 <prax78@gmail.com>
2020-09-22 19:04:01 +0530

    Add files via upload

---
+++ src/arduino/BT.ino
@@ -0,0 +1,35 @@
+
+#include <SoftwareSerial.h>
+SoftwareSerial BTserial(10, 11); // RX | TX
+#define trigPin 13
+#define echoPin 12
+int sensorPin = A0; 
+int sensorValue = 0;
+
+void setup() {
+  
+ BTserial.begin(9600);   
+ pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT); 
+}
+
+void loop() {
+
+
+  long duration, distance;
+  digitalWrite(trigPin, LOW);  // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(trigPin, HIGH);
+
+  delayMicroseconds(10); // Added this line
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1 / 30;
+
+
+              
+                BTserial.print(distance);  
+                
+
+           delay(1000);  
+}      
